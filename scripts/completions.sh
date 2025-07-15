0115b7969429755984267847160ae5681f5bc94e
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-07-15 20:45:25 +0800

    misc: removed CD cli

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -2,7 +2,6 @@
 set -e
 rm -rf completions
 mkdir completions
-cd cli
 for sh in bash zsh fish; do
 	go run . completion "$sh" > "../completions/infisical.$sh"
 done
--- scripts/manpages.sh
+++ scripts/manpages.sh
@@ -2,5 +2,4 @@
 set -e
 rm -rf manpages
 mkdir manpages
-cd cli
 go run . man | gzip -c > "../manpages/infisical.1.gz"
