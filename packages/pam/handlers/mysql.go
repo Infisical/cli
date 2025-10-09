fecfbd92cc786810eae7620b06b4a01ec3cdbaa6
Fang-Pen Lin <fangpen@infisical.com>
2025-10-08 17:04:06 -0700

    More initial code

--- packages/pam/handlers/mysql.go
+++ packages/pam/handlers/mysql.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"context"
 	"crypto/tls"
+	"github.com/Infisical/infisical-merge/packages/pam/session"
+	"net"
+	"sync"
 	"time"
 )
 
+// TODO: DRY with psql?
 type MysqlProxyConfig struct {
 	TargetAddr     string
 	InjectUsername string
@@ -15,4 +20,25 @@ type MysqlProxyConfig struct {
 	SessionID      string
 	EncryptionKey  string
 	ExpiresAt      time.Time
+}
+
+type MysqlProxy struct {
+	config        MysqlProxyConfig
+	sessionLogger *session.SessionLogger
+	mutex         sync.Mutex
+	// TODO:
+}
+
+func NewMysqlProxy(config MysqlProxyConfig, sessionLogger *session.SessionLogger) *MysqlProxy {
+	// TODO:
+	return &MysqlProxy{
+		config:        config,
+		sessionLogger: sessionLogger,
+	}
+}
+
+func (p *MysqlProxy) HandleConnection(ctx context.Context, clientConn net.Conn) error {
+	defer clientConn.Close()
+	// TODO:
+	return nil
 }
