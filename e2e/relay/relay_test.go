229616408ee75aaccd27dcaf677064b8565f833a
Fang-Pen Lin <hello@fangpenlin.com>
2025-11-26 12:07:59 -0800

    Add makefile

--- e2e/relay/relay_test.go
+++ e2e/relay/relay_test.go
@@ -2,10 +2,13 @@ package relay_test
 
 import (
 	"context"
+	"fmt"
+	"github.com/infisical/cli/e2e-tests/packages/client"
 	"github.com/infisical/cli/e2e-tests/packages/infisical"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redis/go-redis/v9"
+	"net/http"
 )
 
 var _ = Describe("Relay", func() {
@@ -14,6 +17,23 @@ var _ = Describe("Relay", func() {
 
 		container := infisical.NewContainers()
 		container.Up()
+
+		hc := http.Client{}
+		cc, err := client.NewClientWithResponses("http://localhost:4000", client.WithHTTPClient(&hc))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print(cc)
+
+		res, err := cc.PostApiV1AdminSignupWithResponse(context.TODO(), client.PostApiV1AdminSignupJSONRequestBody{
+			Email:     "fangpen@infisical.com",
+			FirstName: "Fangpen",
+			Password:  "123456",
+		})
+		fmt.Println(res.StatusCode())
+		fmt.Println(res.JSON200.Token)
+		fmt.Println(res.JSON200.Message)
+
 		////c, err := compose.NewDockerCompose("compose.yaml") // or multiple files: "file1.yml", "file2.yml"
 		//assert.NoError(currentT, err)
 		//
