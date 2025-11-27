364ad7e885bc5005852189f0416236cd884f4fcf
Fang-Pen Lin <hello@fangpenlin.com>
2025-11-26 17:26:58 -0800

    Remove unused stuff

--- e2e/relay/relay_test.go
+++ e2e/relay/relay_test.go
@@ -3,38 +3,60 @@ package relay_test
 import (
 	"context"
 	"fmt"
-	"github.com/infisical/cli/e2e-tests/packages/client"
 	"github.com/infisical/cli/e2e-tests/packages/infisical"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redis/go-redis/v9"
-	"net/http"
+	"github.com/stretchr/testify/assert"
 )
 
 var _ = Describe("Relay", func() {
 	BeforeEach(func() {
 		//ctx := context.Background()
+		stack := infisical.NewStack(infisical.WithDefaultStackFromEnv())
+		dockerCompose, err := stack.ToComposeWithWaitingForService()
+		assert.NoError(currentT, err)
 
-		container := infisical.NewContainers()
-		container.Up()
+		fmt.Println(dockerCompose)
+		err = dockerCompose.Up(context.TODO())
+		assert.NoError(currentT, err)
 
-		hc := http.Client{}
-		cc, err := client.NewClientWithResponses("http://localhost:4000", client.WithHTTPClient(&hc))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(cc)
+		/*
+			hc := http.Client{}
+			cc, err := client.NewClientWithResponses("http://localhost:4000", client.WithHTTPClient(&hc))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Print(cc)
 
-		res, err := cc.PostApiV1AdminSignupWithResponse(context.TODO(), client.PostApiV1AdminSignupJSONRequestBody{
-			Email:     "fangpen@infisical.com",
-			FirstName: "Fangpen",
-			Password:  "123456",
-		})
-		fmt.Println(res.StatusCode())
-		fmt.Println(res.JSON200.Token)
-		fmt.Println(res.JSON200.Message)
+			res, err := cc.PostApiV1AdminSignupWithResponse(context.TODO(), client.PostApiV1AdminSignupJSONRequestBody{
+				Email:     "fangpen@infisical.com",
+				FirstName: "Fangpen",
+				Password:  "123456",
+			})
+			fmt.Println(res.StatusCode())
+			fmt.Println(res.JSON200.Token)
+			fmt.Println(res.JSON200.Message)
 
-		////c, err := compose.NewDockerCompose("compose.yaml") // or multiple files: "file1.yml", "file2.yml"
+			bearerAuth, err := securityprovider.NewSecurityProviderBearerToken(res.JSON200.Token)
+			if err != nil {
+				panic(err)
+			}
+			res2, err := cc.PostApiV3AuthSelectOrganizationWithResponse(
+				context.TODO(),
+				client.PostApiV3AuthSelectOrganizationJSONRequestBody{
+					OrganizationId: res.JSON200.Organization.Id.String(),
+				},
+				bearerAuth.Intercept,
+			)
+
+			fmt.Println(res2.StatusCode())
+			fmt.Println(res2.JSON200.Token)
+
+			res3, err := cc.PostApiV1AuthTokenWithResponse(context.TODO(), bearerAuth.Intercept)
+			fmt.Println(res3.StatusCode())
+			fmt.Println(res3.JSON200.Token)*/
+
 		//assert.NoError(currentT, err)
 		//
 		//err = c.Up(context.Background())
