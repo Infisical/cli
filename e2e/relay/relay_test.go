84a41bf2b2619f3e0cdbd1a74495a969bff242cd
Fang-Pen Lin <hello@fangpenlin.com>
2025-11-27 13:04:52 -0800

    Register id

--- e2e/relay/relay_test.go
+++ e2e/relay/relay_test.go
@@ -2,6 +2,7 @@ package relay_test
 
 import (
 	"context"
+	"github.com/go-faker/faker/v4"
 	"github.com/infisical/cli/e2e-tests/packages/client"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -16,12 +17,32 @@ var _ = Describe("Relay", func() {
 		infisical.Up(context.Background())
 	})
 
-	It("lists projects", func() {
+	It("registers a relay", func() {
 		ctx := context.Background()
 		c := infisical.ApiClient()
 
-		resp, err := c.GetApiV1ProjectsWithResponse(ctx, &client.GetApiV1ProjectsParams{})
+		// Create machine identity for the relay
+		role := "member"
+		identityResp, err := c.PostApiV1IdentitiesWithResponse(ctx, client.PostApiV1IdentitiesJSONRequestBody{
+			Name:           faker.Name(),
+			Role:           &role,
+			OrganizationId: infisical.ProvisionResult().OrgId,
+		})
 		Expect(err).To(BeNil())
-		Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+		Expect(identityResp.StatusCode()).To(Equal(http.StatusOK))
+
+		// Create auth token for relay CLI
+		identityId := identityResp.JSON200.Identity.Id
+		tokenResp, err := c.PostApiV1AuthTokenAuthIdentitiesIdentityIdTokensWithResponse(
+			ctx,
+			identityId.String(),
+			client.PostApiV1AuthTokenAuthIdentitiesIdentityIdTokensJSONRequestBody{},
+		)
+		Expect(err).To(BeNil())
+		Expect(tokenResp.StatusCode()).To(Equal(http.StatusOK))
+
+		//identityResp, err := c.GetApiV1ProjectsWithResponse(ctx, &client.GetApiV1ProjectsParams{})
+		//Expect(err).To(BeNil())
+		//Expect(identityResp.StatusCode()).To(Equal(http.StatusOK))
 	})
 })
