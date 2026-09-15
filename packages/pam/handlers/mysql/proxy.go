55121fa185b000f17f15e294d4b4c54427fb7375
Andrey <a@lbvn.net>
2026-09-14 22:17:28 -0400

    address greptile

--- packages/pam/handlers/mysql/proxy.go
+++ packages/pam/handlers/mysql/proxy.go
@@ -104,6 +104,8 @@ func (p *MysqlProxy) connectToServer() (*client.Conn, error) {
 		p.config.InjectPassword,
 		p.config.InjectDatabase,
 		func(conn *client.Conn) error {
+			conn.UnsetCapability(mysql.CLIENT_LOCAL_FILES)
+
 			if p.config.EnableTLS {
 				if p.config.TLSConfig == nil {
 					return fmt.Errorf("TLS configuration is required when TLS is enabled")
@@ -116,8 +118,6 @@ func (p *MysqlProxy) connectToServer() (*client.Conn, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to MySQL server: %w", err)
 	}
-
-	conn.UnsetCapability(mysql.CLIENT_LOCAL_FILES)
 
 	return conn, nil
 }
